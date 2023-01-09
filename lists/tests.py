7977fc9115d09e9de5b9b645e1769fd01beefce5
Ximena Remeș <ximenaremes@gmail.com>
2023-01-09 18:06:14 +0200

    Chapter 4 - Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -12,14 +13,32 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')  
-        self.assertEqual(found.func, home_page)
-        
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')  
+    #     self.assertEqual(found.func, home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()  
+    #     response = home_page(request)  
+    #     html = response.content.decode('utf8')  
+    #     # self.assertTrue(html.startswith('<html>'))  
+    #     # self.assertIn('<title>To-Do lists</title>', html)  
+    #     # # self.assertTrue(html.endswith('</html>'))  
+    #     # self.assertTrue(html.strip().endswith('</html>'))
+    #     expected_html = render_to_string('home.html')
+    #     self.assertEqual(html, expected_html)
+
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = home_page(request)  
+        response = self.client.get('/')  
+
         html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.endswith('</html>'))  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        # self.assertTemplateUsed(response, 'home.html')  
+        self.assertTemplateUsed(response, 'wrong.html')
