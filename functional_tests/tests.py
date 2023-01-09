a1d04d955ddaf422b16c65880322a43a4c1932e7
Ximena Remeș <ximenaremes@gmail.com>
2023-01-10 00:13:34 +0200

    Chapter 6 - Improving Functional Tests: Ensuring Isolation and Removing Voodoo Sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):  
+# import unittest
+MAX_WAIT = 10
+
+class NewVisitorTest(LiveServerTestCase):  
 
     def setUp(self):  
         self.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -12,19 +17,36 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn(row_text, [row.text for row in rows])
+
+        start_time = time.time()
+        while True:  
+            try:
+                # table = self.browser.find_element_by_id('id_nothing')
+                table = self.browser.find_element_by_id('id_list_table')  
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                # self.assertIn('foo', [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5)  
 
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
-
+        self.browser.get(self.live_server_url)
+    
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  
         # self.fail('Finish the test!')  
+        
+       
         header_text = self.browser.find_element_by_tag_name('h1').text  
         self.assertIn('To-Do', header_text)
 
@@ -44,8 +66,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -53,11 +75,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
        
         
         # table = self.browser.find_element_by_id('id_list_table')
@@ -85,8 +107,8 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight away
         # [...rest of comments as before]
 
-if __name__ == '__main__':  
-    unittest.main()
+# if __name__ == '__main__':  
+#     unittest.main()
 
 # assert 'Django' in browser.title
 # assert 'To-Do' in browser.title, "Browser title was " + browser.title
