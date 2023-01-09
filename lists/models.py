72fe11009a2ff6a18ae68ffa3b849adb1ea264c1
Ximena Remeș <ximenaremes@gmail.com>
2023-01-09 20:48:24 +0200

    Chapter 5 - Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# class Item(object):
+#     pass
+
+class Item(models.Model):
+    # pass
+    # text = models.TextField()
+    text = models.TextField(default='')
