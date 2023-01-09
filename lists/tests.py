23ab7960f1390f31e24bf4365c33438901bacac6
Ximena Remeș <ximenaremes@gmail.com>
2023-01-09 20:05:19 +0200

    Chapter 5 - Three Strikes and Refactor

--- lists/tests.py
+++ lists/tests.py
@@ -21,6 +21,12 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+
     # def test_home_page_returns_correct_html(self):
     #     request = HttpRequest()  
     #     response = home_page(request)  
@@ -32,13 +38,11 @@ class HomePageTest(TestCase):
     #     expected_html = render_to_string('home.html')
     #     self.assertEqual(html, expected_html)
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')  
-
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        # self.assertTemplateUsed(response, 'home.html')  
-        self.assertTemplateUsed(response, 'wrong.html')
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')  
+    #     html = response.content.decode('utf8')  
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+    #     # self.assertTemplateUsed(response, 'home.html')  
+    #     self.assertTemplateUsed(response, 'wrong.html')
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 # home_page = None
 def home_page(request):
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
+
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    # return render(request, 'home.html')
+
+    return render(request, 'home.html', {
+        # 'new_item_text': request.POST['item_text'],
+        'new_item_text': request.POST.get('item_text', ''),
+    })
