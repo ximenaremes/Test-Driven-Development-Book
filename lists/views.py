e8eeea1ac080ec2ecf45abdfa1cc150601ffdcbb
Ximena Remeș <ximenaremes@gmail.com>
2023-01-10 20:58:56 +0200

    Chapter 7 - The Functional Tests Detect Another Regression

--- lists/views.py
+++ lists/views.py
@@ -40,15 +40,12 @@ def home_page(request):
 
   
     
-def view_list(request):
-    # pass
-    items = Item.objects.all()
-    # return render(request, 'home.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
-
-
+    return redirect(f'/lists/{list_.id}/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -29,8 +29,13 @@ from lists import views
 #     url(r'^$', views.home_page, name='home'),
 #     url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 # ]
+# urlpatterns = [
+#     url(r'^$', views.home_page, name='home'),
+#     url(r'^lists/new$', views.new_list, name='new_list'),
+#     url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+# ]
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^lists/(.+)/$', views.view_list, name='view_list'),
 ]
