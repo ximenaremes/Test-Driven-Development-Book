04d85a8ade653156479b3fc79735c14377d5d018
Ximena Remeș <ximenaremes@gmail.com>
2023-01-10 20:33:44 +0200

    Chapter 7 - Biting the Bullet: Adjusting Our Models

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
-
+from lists.models import Item, List
 # Create your tests here.
 
 class ListViewTest(TestCase):
@@ -14,13 +13,18 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        # Item.objects.create(text='itemey 1')
+        # Item.objects.create(text='itemey 2')
 
-        self.assertContains(response, 'itemey 1')  
-        self.assertContains(response, 'itemey 2')
+        # response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        # self.assertContains(response, 'itemey 1')  
+        # self.assertContains(response, 'itemey 2')
 
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
@@ -35,24 +39,35 @@ class ListViewTest(TestCase):
         # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
+
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
     # def test_can_save_a_POST_request(self):
     #     response = self.client.post('/', data={'item_text': 'A new list item'})
